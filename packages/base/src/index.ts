e2192ed976316cc85cd2665a93700600a22dbd56
Shuntaro Nishizawa <me@shun.technology>
2023-03-28 02:49:24 +0900

    fix(base): Disable some unicorn recommended rules

--- packages/base/src/index.ts
+++ packages/base/src/index.ts
@@ -114,18 +114,35 @@ export default defineConfig({
     "require-unicode-regexp": "warn",
     // シンボルに名前を付ける
     "symbol-description": "warn",
+
     // import/export の順序を整理
     "simple-import-sort/imports": "warn",
     "simple-import-sort/exports": "warn",
+
     // unicornの推奨ルールから不要なものを無効化
+    // ファイルの名前はコンポーネントなどで形式が変化するため無効化
     "unicorn/filename-case": "off",
+    // コンポーネント内の関数など、スコープを小さくしておきたい場合があるので無効化
     "unicorn/consistent-function-scoping": "off",
+    // nullも使う場合があるので無効化
     "unicorn/no-null": "off",
+    // `.forEach()` のほうが簡潔に書ける場合があるので無効化
     "unicorn/no-array-for-each": "off",
+    // 否定形の方が簡潔に書ける場合があるので無効化
     "unicorn/no-negated-condition": "off",
-    "unicorn/prefer-module": "off",
-    "unicorn/prevent-abbreviations": "off",
+    // 直接関数を渡したほうが簡潔に書ける場合があるので無効化
     "unicorn/no-array-callback-reference": "off",
+    // CommonJSを使う場合もあるため無効化
+    "unicorn/prefer-module": "off",
+    // `[...array].map()` は ~~ダサい~~ ので無効化
+    "unicorn/prefer-spread": "off",
+    // セレクタは使い分けたいので無効化
+    "unicorn/prefer-query-selector": "off",
+    // 略語の方が簡潔に書ける場合があるので無効化
+    "unicorn/prevent-abbreviations": "off",
+    // switch文のcase節を常にブロックにするのは冗長なので必要なときのみブロックにする
+    "unicorn/switch-case-braces": ["error", "avoid"],
+
     // 安全でない正規表現を警告
     "unicorn/no-unsafe-regex": "warn",
     // なるべく repleaseAll を使用する

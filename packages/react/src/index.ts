433b5870ff9e864c8d2d2e3190afa0607ba07d00
Shuntaro Nishizawa <me@shun.technology>
2023-03-29 02:26:35 +0900

    feat(react): Add rules

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -14,12 +14,25 @@ export default defineConfig({
       ],
       settings: {
         react: {
+          // react のバージョンを指定
           version: "18",
         },
       },
       rules: {
+        // propTypes はTSで代替できるので無効化
         "react/prop-types": "off",
+        // HTMLの属性名として認識されていない属性名を許可
+        // TSで型チェックしているので不要
         "react/no-unknown-property": "off",
+        // `onClick` などのイベントハンドラーの命名規則をチェック
+        "react/jsx-handler-names": "warn",
+        // `React.Fragment` を省略可能な場合は省略する
+        // ただし、フラグメントのみの場合は許可する
+        "react/jsx-no-useless-fragment": ["warn", { allowExpressions: true }],
+        // rel 属性の値をチェック
+        "react/no-invalid-html-attribute": "warn",
+        // コンポーネント内でコンポーネントを定義するのを許可しない
+        "react/no-unstable-nested-components": "error",
       },
     },
   ],

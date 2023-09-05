06c55a0d75968df6138fc4879ec20336c84073d5
Shuntaro Nishizawa <me@s2n.tech>
2023-09-06 03:16:31 +0900

    fix(react): Fix files config

--- packages/react/src/configs/react-hooks.js
+++ packages/react/src/configs/react-hooks.js
@@ -4,7 +4,6 @@ import reactHooks from "eslint-plugin-react-hooks";
  * @type {import("@shun-shobon/eslint-config-utils").Config}
  */
 const config = {
-  files: ["**/*.jsx", "**/*.tsx"],
   plugins: {
     "react-hooks": reactHooks,
   },

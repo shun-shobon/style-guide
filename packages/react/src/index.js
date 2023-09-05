472a3e9d67949cdd47a225624af64e66949ab5b6
Shuntaro Nishizawa <me@s2n.tech>
2023-09-06 01:34:50 +0900

    refactor: Prune packages

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -1,6 +1,6 @@
+import jsxA11yConfig from "./configs/jsx-a11y.js";
 import reactConfig from "./configs/react.js";
 import reactHooksConfig from "./configs/react-hooks.js";
-import jsxA11yConfig from "./configs/jsx-a11y.js";
 
 /**
  * @type {import("@shun-shobon/eslint-config-utils").Config[]}
--- packages/utils/src/index.d.ts
+++ packages/utils/src/index.d.ts
@@ -1,10 +1,4 @@
-import type { TSESLint } from "@typescript-eslint/utils";
-
-export function defineConfig<T extends TSESLint.Linter.Config>(config: T): T {
-  return config;
-}
-
-interface LanguageOptions {
+export interface LanguageOptions {
   ecmaVersion?: number | "latest";
   sourceType?: "module" | "commonjs" | "script";
   globals: Record<string, "readonly" | "writable" | "off">;
@@ -12,13 +6,13 @@ interface LanguageOptions {
   parserOptions?: Record<string, unknown>;
 }
 
-interface LinterOptions {
+export interface LinterOptions {
   noInlineConfig: boolean;
   reportUnusedDisableDirectives: boolean;
 }
 
-type Rule = "off" | "warn" | "error";
-type RuleConfig = Rule | [Rule, ...Array<unknown>];
+export type Rule = "off" | "warn" | "error";
+export type RuleConfig = Rule | [Rule, ...Array<unknown>];
 
 export interface Config {
   files?: Array<string>;

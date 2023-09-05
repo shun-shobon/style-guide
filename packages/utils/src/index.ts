d543651b766fb0d1efc6ee5e2ea367b589a8579e
Shuntaro Nishizawa <me@s2n.tech>
2023-09-05 15:50:22 +0900

    feat(utils): Add flat config type

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -3,3 +3,13 @@ import type { TSESLint } from "@typescript-eslint/utils";
 export function defineConfig<T extends TSESLint.Linter.Config>(config: T): T {
   return config;
 }
+
+type Rule = "off" | "warn" | "error";
+type RuleConfig = Rule | [Rule, ...Array<unknown>];
+
+export interface Config {
+  files: Array<string>;
+  ignores: Array<string>;
+  plugins: Record<string, unknown>;
+  rules: Record<string, RuleConfig>;
+}

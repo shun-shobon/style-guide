a0c4fe2ab53b089294fbd3ed3ec0d75bc1852934
Shuntaro Nishizawa <me@s2n.tech>
2023-12-12 10:51:22 +0900

    feat: Add rules/plugins exports

--- packages/javascript/src/index.ts
+++ packages/javascript/src/index.ts
@@ -1,21 +1,23 @@
-import { defineConfig } from "@shun-shobon/eslint-config-utils";
+import { type Config, defineConfig } from "@shun-shobon/eslint-config-utils";
 import { GLOB_SRC } from "@shun-shobon/eslint-config-utils/globs";
 
 import * as basic from "./basic";
 import * as imports from "./imports";
 import * as unicorn from "./unicorn";
 
-export { basic, imports };
+export const plugins: Config.Plugins = {
+  ...imports.plugins,
+  ...unicorn.plugins,
+};
+
+export const rules: Config.Rules = {
+  ...basic.rules,
+  ...imports.rules,
+  ...unicorn.rules,
+};
 
 export default defineConfig({
   files: [GLOB_SRC],
-  plugins: {
-    ...imports.plugins,
-    ...unicorn.plugins,
-  },
-  rules: {
-    ...basic.rules,
-    ...imports.rules,
-    ...unicorn.rules,
-  },
+  plugins,
+  rules,
 });

c82842cf589636936ead057af8bdd28c106c9c4d
Shuntaro Nishizawa <me@s2n.tech>
2023-12-13 22:37:32 +0900

    refactor(react): Remove js files

--- packages/react/src/flat.js
+++
@@ -1,8 +0,0 @@
-import jsxA11yConfig from "./configs/jsx-a11y.js";
-import reactConfig from "./configs/react.js";
-import reactHooksConfig from "./configs/react-hooks.js";
-
-/**
- * @type {import("@shun-shobon/eslint-config-utils").Config[]}
- */
-export default [reactConfig, reactHooksConfig, jsxA11yConfig];

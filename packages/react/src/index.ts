6b5860821323c2d1cf11b7ab4df0cc1812f1467f
Shuntaro Nishizawa <me@s2n.tech>
2023-12-13 22:37:15 +0900

    feat(react): Add jsx a11y rules

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -7,10 +7,15 @@ import { GLOB_JSX } from "@shun-shobon/eslint-config-utils/globs";
 
 import * as react from "./react";
 import * as reactHooks from "./react-hooks";
+import * as jsxA11y from "./jsx-a11y";
 
-export const plugins = combinePlugins(react.plugins, reactHooks.plugins);
+export const plugins = combinePlugins(
+  react.plugins,
+  reactHooks.plugins,
+  jsxA11y.plugins
+);
 
-export const rules = combineRules(react.rules, reactHooks.rules);
+export const rules = combineRules(react.rules, reactHooks.rules, jsxA11y.rules);
 
 export default defineConfig({
   files: [GLOB_JSX],
---
+++ packages/react/src/jsx-a11y.ts
@@ -0,0 +1,29 @@
+import {
+  definePlugins,
+  renameAlias,
+  combineRules,
+  defineRules,
+} from "@shun-shobon/eslint-config-utils";
+import type { Config } from "@shun-shobon/eslint-config-utils";
+// @ts-expect-error: This package don't have types
+import jsxA11y from "eslint-plugin-jsx-a11y";
+
+export const ALIAS_JSX_A11Y = "jsx-a11y";
+
+export const plugins = definePlugins(ALIAS_JSX_A11Y, jsxA11y as Config.Plugin);
+
+const baseRules = renameAlias(
+  ALIAS_JSX_A11Y,
+  jsxA11y.configs.recommended.rules as Config.Rules
+);
+
+const additionalRules = defineRules(ALIAS_JSX_A11Y, {
+  // 曖昧なリンクのテキストを許可しない
+  "anchor-ambiguous-text": "error",
+  // インタラクティブな要素にラベルが付いていないことを許可しない
+  "control-has-associated-label": "error",
+  // フォーカス可能な要素に `aria-hidden` 属性を付与することを許可しない
+  "no-aria-hidden-on-focusable": "error",
+});
+
+export const rules = combineRules(baseRules, additionalRules);

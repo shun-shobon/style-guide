f8f7c35ffdd16c313ae9fc0ae5fef332f30ce304
Shuntaro Nishizawa <me@s2n.tech>
2023-12-14 14:36:32 +0900

    feat(eslint-config): Add config type

---
+++ packages/eslint-config/src/types.ts
@@ -0,0 +1,10 @@
+import type { FlatESLintConfig, Rules } from "eslint-define-config";
+
+export interface ConfigItem
+  extends Omit<FlatESLintConfig, "plugins" | "rules"> {
+  /**
+   * Relax plugins type limitation, as most of the plugins did not have correct type info yet.
+   */
+  plugins?: Record<string, unknown>;
+  rules?: Rules;
+}

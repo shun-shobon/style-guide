46470efa01631ed43d280645b79c413adbb9f64b
Shuntaro Nishizawa <me@s2n.tech>
2023-12-14 11:09:33 +0900

    chore: Remove unnecessary package

--- eslint.config.js
+++
@@ -1,5 +0,0 @@
-import javascript from "@shun-shobon/eslint-config-javascript";
-import typescript from "@shun-shobon/eslint-config-typescript";
-import { defineConfig } from "@shun-shobon/eslint-config-utils";
-
-export default defineConfig(...javascript, ...typescript);

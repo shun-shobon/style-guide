832b432aa9b01ebaa03cdeaf160c6e64bd7474bc
Shuntaro Nishizawa <me@s2n.tech>
2023-12-15 23:09:21 +0900

    feat(prettier-config): Add package.json sort plugin

--- packages/prettier-config/src/index.ts
+++ packages/prettier-config/src/index.ts
@@ -1,34 +1,29 @@
+import { createRequire } from "node:module";
+
 import type { Config } from "prettier";
 
-export function shun_shobon(userConfig: Config): Config {
+const require = createRequire(import.meta.url);
+
+export function shun_shobon(userConfig: Config = {}): Config {
 	const config: Config = {
 		useTabs: true,
 		quoteProps: "consistent",
 
 		// This is the default, but VSCode's Prettier plugin doesn't respect it.
 		trailingComma: "all",
+
+		plugins: [require.resolve("prettier-plugin-pkg")],
 	};
 
 	return mergeConfig(config, userConfig);
 }
 
-function mergeConfig(config1: Config, config2: Config): Config {
-	const {
-		overrides: overrides1,
-		plugins: plugins1,
-		...otherConfigs1
-	} = config1;
-	const {
-		overrides: overrides2,
-		plugins: plugins2,
-		...otherConfigs2
-	} = config2;
-
-	return {
-		...otherConfigs1,
-		...otherConfigs2,
-
-		overrides: [...(overrides1 ?? []), ...(overrides2 ?? [])],
-		plugins: [...(plugins1 ?? []), ...(plugins2 ?? [])],
-	};
+function mergeConfig(...configs: Config[]): Config {
+	return configs.reduce<Config>((acc, config) => {
+		return {
+			...acc,
+			...config,
+			plugins: [...(acc.plugins ?? []), ...(config.plugins ?? [])],
+		};
+	}, {});
 }

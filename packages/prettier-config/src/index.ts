346b769b94a658cbe73db9d5c0a8b97a7e18d729
Shuntaro Nishizawa <me@s2n.tech>
2023-12-15 13:19:05 +0900

    feat(prettier-config): Add config

--- packages/prettier-config/src/index.ts
+++ packages/prettier-config/src/index.ts
@@ -1,1 +1,15 @@
-export {};
+import type { Config } from "prettier";
+
+export function shun_shobon(userConfig: Config): Config {
+  const config: Config = {
+    quoteProps: "consistent",
+
+    // This is the default, but VSCode's Prettier plugin doesn't respect it.
+    trailingComma: "all",
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+}

043d5539e655e4055dd40d5a75e50e9c8ed2da4c
Shuntaro Nishizawa <me@s2n.tech>
2023-12-15 19:02:50 +0900

    feat(prettier-config): Add merge config

--- packages/prettier-config/src/index.ts
+++ packages/prettier-config/src/index.ts
@@ -9,8 +9,26 @@ export function shun_shobon(userConfig: Config): Config {
 		trailingComma: "all",
 	};
 
+	return mergeConfig(config, userConfig);
+}
+
+function mergeConfig(config1: Config, config2: Config): Config {
+	const {
+		overrides: overrides1,
+		plugins: plugins1,
+		...otherConfigs1
+	} = config1;
+	const {
+		overrides: overrides2,
+		plugins: plugins2,
+		...otherConfigs2
+	} = config2;
+
 	return {
-		...config,
-		...userConfig,
+		...otherConfigs1,
+		...otherConfigs2,
+
+		overrides: [...(overrides1 ?? []), ...(overrides2 ?? [])],
+		plugins: [...(plugins1 ?? []), ...(plugins2 ?? [])],
 	};
 }

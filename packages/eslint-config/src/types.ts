48357782519f71a507d2c0dbd28c2fc1532aeb99
Shuntaro Nishizawa <me@s2n.tech>
2023-12-16 00:16:54 +0900

    style: Apply prettier

--- packages/eslint-config/src/types.ts
+++ packages/eslint-config/src/types.ts
@@ -5,7 +5,8 @@ export type Awaitable<T> = T | Promise<T>;
 
 export interface ConfigItem extends Omit<FlatESLintConfig, "plugins"> {
 	/**
-	 * Relax plugins type limitation, as most of the plugins did not have correct type info yet.
+	 * Relax plugins type limitation, as most of the plugins did not have correct
+	 * type info yet.
 	 */
 	// eslint-disable-next-line typescript/no-explicit-any
 	plugins?: Record<string, any>;
@@ -17,8 +18,8 @@ export interface OptionsConfig {
 	 *
 	 * Passing an object to configure the options.
 	 *
-	 * @see https://github.com/antfu/eslint-config-flat-gitignore
 	 * @default true
+	 * @see https://github.com/antfu/eslint-config-flat-gitignore
 	 */
 	gitignore?: boolean | FlatGitignoreOptions;
 

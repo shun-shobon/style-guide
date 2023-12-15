d95aff9ac6ec9c6802634c99750c2c59c7892a91
Shuntaro Nishizawa <me@s2n.tech>
2023-12-15 18:45:11 +0900

    feat(prettier-config): Use tab indent

--- packages/prettier-config/src/index.ts
+++ packages/prettier-config/src/index.ts
@@ -1,15 +1,16 @@
 import type { Config } from "prettier";
 
 export function shun_shobon(userConfig: Config): Config {
-  const config: Config = {
-    quoteProps: "consistent",
+	const config: Config = {
+		useTabs: true,
+		quoteProps: "consistent",
 
-    // This is the default, but VSCode's Prettier plugin doesn't respect it.
-    trailingComma: "all",
-  };
+		// This is the default, but VSCode's Prettier plugin doesn't respect it.
+		trailingComma: "all",
+	};
 
-  return {
-    ...config,
-    ...userConfig,
-  };
+	return {
+		...config,
+		...userConfig,
+	};
 }
